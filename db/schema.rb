4381f074deb70a0958972b52a53206d24b3a2ce3
Harris Novick <harrisdavidnovick@gmail.com>
2015-07-05 20:29:45 -0400

    add embedded player url attrs to mixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150705232346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mixes", force: :cascade do |t|
+    t.text     "mixesdb_url"
+    t.text     "soundcloud_url"
+    t.text     "mixcloud_url"
+    t.text     "youtube_url"
+    t.text     "hulkshare_url"
+    t.text     "zippyshare_url"
+    t.text     "hearthisat_url"
+    t.text     "full_title"
+    t.text     "artists",        default: [], array: true
+    t.text     "genres",         default: [], array: true
+    t.text     "venue"
+    t.datetime "air_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "mixes", ["artists"], name: "index_mixes_on_artists", using: :gin
+  add_index "mixes", ["genres"], name: "index_mixes_on_genres", using: :gin
+  add_index "mixes", ["mixesdb_url"], name: "index_mixes_on_mixesdb_url", unique: true, using: :btree
 
 end
