e79bf297065f1248c8856b286bc7b98aa7e49fa6
Harris Novick <harrisdavidnovick@gmail.com>
2015-07-07 16:16:13 -0400

    dl sc with off-the-rip

--- app/models/mix.rb
+++ app/models/mix.rb
@@ -10,4 +10,28 @@ class Mix < ActiveRecord::Base
 
   validates :full_title,  presence: true
   validates :mixesdb_url, presence: true, uniqueness: true
+
+  scope :with_soundcloud, -> { where("array_length(soundcloud_urls, 1) > 0") }
+
+  def self.download_soundcloud_files
+    with_soundcloud.order('air_date DESC').each do |mix|
+      mix.soundcloud_urls.each do |sc_url|
+        begin
+          puts mix.full_title.green
+          puts sc_url.red
+
+          Timeout::timeout(600) do
+            path = "#{Rails.root}/data/audio/soundcloud/#{mix.full_title.parameterize}"
+            FileUtils.mkdir_p(path)
+
+            `otr dl #{sc_url} #{path}`
+          end
+
+          sleep 5
+        rescue => e
+          puts "#{e.class}: #{e.message}".red
+        end
+      end
+    end
+  end
 end
